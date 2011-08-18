0a1059df5b0d2173cd396e2410eb80e20e244855
dominges@dominges.org <dominges@dominges.org@c50fd145-4172-5fe0-d1a2-f48717f96b69>
2011-08-18 10:32:37 +0000

    Control Chat update

--- branches/Kol9yN/chat/control_chat/config.cpp
+++ branches/Kol9yN/chat/control_chat/config.cpp
@@ -1,174 +1,70 @@
-﻿
-class CfgPatches {
+﻿class CfgPatches  {
 
-	class Sln_Chat_1 {
-		units[] = {};
-		weapons[] = {};
-		requiredVersion = 0.100000;
-		requiredAddons[] = {"CAUI"};
-	};
+        class Sln_Chat_1  {
+                units[] = {};
+                weapons[] = {};
+                requiredVersion = 0.100000;
+                requiredAddons[] = {"CAUI";"ace_c_ui" };
+        };
 };
 class RscButton;
 class RscText;
 class RscPicture;
 class RscEdit;
-
-class RscDisplayChat {
+class RscShortcutButton;
+//окно ввода
+class RscDisplayChat  {
 idd = 24;
-	onLoad = "_as = [] spawn Fn_chat_Creat";
-	onUnLoad = "closeDialog 0";
-	onKeyDown = "if ((_this select 1) == 28 || (_this select 1) == 156) then {_as = [] spawn Fn_chat_exit}";
-	class controls {
-	class Line : RscText {idc = 101; x=0;y=0;h=0;w=0;};
-	class Background : RscText {x=0;y=0;h=0;w=0;};
-	class Ca_Line : RscText {idc = 101; x=0;y=0;h=0;w=0;};
-	class Ca_Background : RscText {x=0;y=0;h=0;w=0;};
-	};
-};
-
-class RscDisplayChannel  {
-        idd = 63;
- 
-        class controls  {
-        class Channel;
- 
-    class CA_Channel : RscText {
-                        idc = 101;
-						x=0;y=0;h=0;w=0;
-                };
-    class Background : RscText {
-						x=0;y=0;h=0;w=0;
-                };
-    class Picture : RscPicture {
-						x=0;y=0;h=0;w=0;
-                };
-        };
-};
-
-class Sln_ChatIn_N3 {
-idd = 5063;
+onLoad = "_as = _this execVM '\control_chat\e_o.sqf'";
 movingEnable = true;
 enableSimulation = true;
-class controls  {
-
-class Channel : RscText {
-			idc = 50100;
-			x = "SafeZoneX + SafeZoneW*0.01";
-			y = "SafeZoneY + SafeZoneH*0.9";
-			w = "SafeZoneW*0.15";
-			h = "SafeZoneH*0.03";
-			colorText[] = {1,1,1,1 };
-			colorBackground[] = {0.188200,0.258800,0.149000,0.760000 };
-			text = "";
-			};
-class edit : Channel {
-			idc = 50101;
-			type = 2;
-			style = 0;
-			sizeEx = 0.034000;
-			x = "SafeZoneX + SafeZoneW*0.17";
-			y = "SafeZoneY + SafeZoneH*0.9";
-			//Добавлено
-			w = "SafeZoneW*0.3";
-			h = "SafeZoneH*0.03";
-			colorSelection[] = {1,1,1,0.850000};
-			autocomplete = "general";
-		};
-
-class bt : RscButton {
-			idc = 50200;
-			style = 0;
-			x = "SafeZoneX + SafeZoneW*0.01";
-			y = "SafeZoneY + SafeZoneH*0.8";
-			w = "SafeZoneW*0.05";
-			h = "SafeZoneH*0.03";
-			sizeEx = 0.03;
-			color[] = {0.543000,0.574200,0.410200,1.000000 };
-			color2[] = {0.950000,0.950000,0.950000,1 };
-			colorBackground[] = {1,1,1,1 };
-			colorbackground2[] = {1,1,1,0.400000 };
-			colorDisabled[] = {1,1,1,0.250000 };
-			periodFocus = 1.200000;
-			periodOver = 0.800000;
-			text = "Admin";
-			action = "_as = [] spawn Fn_chat_admin";
-		};
-	};
+        onKeyDown = "if ((_this select 1) == 28) then {_as = (ctrlText ((_this select 0) displayCtrl 101)) execVM '\control_chat\edit.sqf'}";
+        class controls  {
+        class Ca_Line;
+        class Ca_Background;
+        };
 };
-
+//название канала
+class RscDisplayChannel  {
+idd = 63;
+onLoad = "if (isNil 'Sln_Cat_Mod') then {_as = [0] execVM '\control_chat\n_o.sqf'}";
+        class controls  {
+                class Ca_Channel;
+                class Background ;
+                class Picture;
+        };
+};
+// окно админа
 class Sln_Chat_admin {
 idd = 5064;
 movingEnable = true;
 enableSimulation = true;
 class controls  {
-class bt0 : RscButton {
-			idc = 50200;
-			style = 0;
-			x = "SafeZoneX + SafeZoneW*0.01";
-			y = "SafeZoneY + SafeZoneH*0.8";
-			w = "SafeZoneW*0.2";
-			h = "SafeZoneH*0.025";
-			text = "Общий доступ";
-			action = "_as = 0 spawn Fn_chat_select";
-		};
+class bt0 : RscShortcutButton {
+                        idc = 50200;
+                        x = "SafeZoneX + SafeZoneW*0.01";
+                        y = "SafeZoneY + SafeZoneH*0.55";
+                        w = "SafeZoneW*0.2";
+                        h = "SafeZoneH*0.1";
+                        text = "Общий";
+                        action = "_as = 0 spawn Fn_chat_select";
+                };
 class bt1 : bt0 {
-		idc = 50201;
-		text = "Без раций";
-		y = "SafeZoneY + SafeZoneH*0.829";
-		action = "_as = 1 spawn Fn_chat_select";
-		};
+                idc = 50201;
+                text = "Канал стороны";
+                y = "SafeZoneY + SafeZoneH*0.6";
+                action = "_as = 1 spawn Fn_chat_select";
+                };
 class bt2 : bt0 {
-		idc = 50202;
-		text = "Для трупов";
-		y = "SafeZoneY + SafeZoneH*0.858";
-		action = "_as = 2 spawn Fn_chat_select";
-		};
-class bt3 : bt0 {
-		y = "SafeZoneY + SafeZoneH*0.887";
-		text = "Exit";
-		action = "closeDialog 0";
-		};
-	};
-};
-
-class RscTitles
-{
-	titles[]={}; 
-
-class Sln_Chat_Out_N3 {
-idd = 5064;
-movingEnable = true;
-enableSimulation = true;
-duration=500;
-fadein=0;
-fadeOut=1;
-onLoad = "_as = _this Spawn Fn_chat_Tim";
-class controls
-		{
-class Sln_List : RscText
-			{
-			idc = 50102;
-			style = 16;
-			lineSpacing = 1;
-			x = "SafeZoneX + SafeZoneW*0.01";
-			y = "SafeZoneY + SafeZoneH*0.3";
-			w = "SafeZoneW*0.3";
-			h = "SafeZoneH*0.5";
-			colorText[] = {1,1,1,1 };
-			text = "CHAT";
-			};
-		};
-	};
-};
-
-class CfgVehicles  {
-	class Logic;
-	class Sln_Management_Chat  : Logic {
-		displayName = "CHAT";
-		vehicleClass = "Modules";
-
-		class Eventhandlers  {
-			init = "(_this select 0) execVM '\control_chat\serv.sqf';";
-		};
-	};
+                idc = 50202;
+                text = "Канал группы";
+                y = "SafeZoneY + SafeZoneH*0.65";
+                action = "_as = 2 spawn Fn_chat_select";
+                };
+class btex : bt0 {
+                y = "SafeZoneY + SafeZoneH*0.7";
+                text = "Выход";
+                action = "closeDialog 0";
+                };
+        };
 };
---
+++ branches/Kol9yN/chat/control_chat/n_o.sqf
@@ -0,0 +1,21 @@
+﻿if isServer then {
+        Sln_Cat_Mod = "HeliHEmpty" createVehicle [0,0];
+        publicVariable "Sln_Cat_Mod";
+        Sln_Cat_Mod setVariable ["Sln_Chat_State", [true,true,true], true];
+} else {
+        Sln_Cat_Mod = call compile "Sln_Cat_Mod";
+};
+                
+Fn_chat_select = {
+        disableSerialization;
+        _ms = Sln_Cat_Mod getVariable "Sln_Chat_State";
+        ((findDisplay 5064) displayCtrl (50200+_this)) ctrlSetTextColor
+        (
+        if (_ms select _this) then {
+                _ms set [_this, false]; [1,0,0,1]
+                } else {
+                _ms set [_this, true]; [0,1,0,1]
+                }
+        );
+Sln_Cat_Mod setVariable ["Sln_Chat_State", _ms, true];
+};
